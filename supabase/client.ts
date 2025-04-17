31e49118158a0f1132f3678b403b15c9e9df8914
marcos6042 <marcosmachado6042@gmail.com>
2025-04-17 09:39:30 -0300

    Update client.ts

--- supabase/client.ts
+++ supabase/client.ts
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://vfypfmxvaukzsmkxupcr.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmeXBmbXh2YXVrenNta3h1cGNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3NTkwMzcsImV4cCI6MjA2MDMzNTAzN30.v7oHWqlP7KusJ_7TsrtRMwPTWEswQQI5dTSJzQ1US-0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
