ea3a1c0f6ce7dcd1fba5e5b6d23f0302633c99b8
marcos6042 <marcosmachado6042@gmail.com>
2025-05-08 00:28:05 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-purple-700">OneFlow Contábil</h1>
+      <p>Bem-vindo ao sistema de importação e lançamento contábil.</p>
+    </div>
+  );
+}
